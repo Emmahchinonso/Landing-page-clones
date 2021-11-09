89ead41ee849161c526b81a0fee88d7d13d9bd49
Emmahchinonso <emmanuelchinons9@gmail.com>
2021-11-09 05:48:55 +0100

    change contianer and section text styles

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,21 @@
 const menuButton = document.querySelector('.js-menu-btn');
 const nav = document.querySelector('.js-nav');
+const header = nav.parentElement;
 const moreBtn = document.querySelector('.js-more-btn');
 const moreContainer = document.querySelector('.js-more');
 const secondaryNav = document.querySelector('.js-nav-secondary')
 
+console.log()
+
 menuButton.addEventListener('click', () => {
   nav.classList.toggle('isOpen')
+  if(Array.from(nav.classList).includes('isOpen')) {
+    secondaryNav.style.visibility = 'hidden';
+  } else {
+    setTimeout(() => {
+      secondaryNav.style.visibility = 'visible';
+    }, 300)
+  }
 })
 
 moreBtn.addEventListener('click', () => {
